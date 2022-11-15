deab77a50dfc36f38959f59705fc7d9baa750ad8
Hennath <henning.athmer@gmail.com>
2022-11-15 11:41:36 +0100

    refactor: adjusting some variable names

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class CodeForm(FlaskForm):
-    user_code = StringField('Mitarbeiternummer', validators=[DataRequired()])
+    user_code = StringField('Personalnummer', validators=[DataRequired()])
     submit = SubmitField('Anmelden')
 
