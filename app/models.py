766019cf39595064e7e5a09a07b8358ccc1cbf51
Hennath <henning.athmer@gmail.com>
2022-11-15 09:42:58 +0100

    Added columns to the models

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    user_code = db.Column(db.Integer, index=True, unique=True)
+    personalnummer = db.Column(db.Integer, index=True, unique=True)
 
     def __repr__(self):
         return f"User {self.username}"
@@ -13,6 +13,8 @@ class Buchungen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    kommen = db.Column(db.DateTime, default=datetime.utcnow)
+    gehen = db.Column(db.DateTime)
 
     def __repr__(self):
         return f"Gestempelt um: {self.timestamp}"
